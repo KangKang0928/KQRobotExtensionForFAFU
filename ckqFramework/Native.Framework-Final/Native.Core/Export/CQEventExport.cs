05768ee992cc061b281a9b43b02f07c890f07483
李兆祥 <393727164@qq.com>
2020-06-19 01:46:49 +0800

    init project

---
+++ ckqFramework/Native.Framework-Final/Native.Core/Export/CQEventExport.cs
@@ -0,0 +1,143 @@
+/*
+ * 此文件由T4引擎自动生成, 请勿修改此文件中的代码!
+ */
+using System;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Native.Core;
+using Native.Core.Domain;
+using Native.Sdk.Cqp;
+using Native.Sdk.Cqp.Enum;
+using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.Interface;
+using Native.Sdk.Cqp.Expand;
+using Native.Sdk.Cqp.Model;
+using Unity;
+using Unity.Injection;
+
+namespace Native.App.Export
+{
+	/// <summary>	
+	/// 表示酷Q事件导出的类	
+	/// </summary>	
+	public class CQEventExport	
+	{	
+		#region --构造函数--	
+		/// <summary>	
+		/// 由托管环境初始化的 <see cref="CQEventExport"/> 的新实例	
+		/// </summary>	
+		static CQEventExport ()	
+		{	
+			// 初始化 Costura.Fody	
+			CosturaUtility.Initialize ();	
+			
+			Type appDataType = typeof (AppData);	
+			appDataType.GetRuntimeProperty ("UnityContainer").GetSetMethod (true).Invoke (null, new object[] { new UnityContainer () });	
+			// 调用方法进行注册	
+			CQMain.Register (AppData.UnityContainer);	
+			
+			// 调用方法进行实例化	
+			ResolveBackcall ();	
+		}	
+		#endregion	
+		
+		#region --核心方法--	
+		/// <summary>	
+		/// 返回酷Q用于识别本应用的 AppID 和 ApiVer	
+		/// </summary>	
+		/// <returns>酷Q用于识别本应用的 AppID 和 ApiVer</returns>	
+		[DllExport (ExportName = "AppInfo", CallingConvention = CallingConvention.StdCall)]	
+		private static string AppInfo ()	
+		{	
+			return "9,cn.zxlee.manageforfafu";	
+		}	
+		
+		/// <summary>	
+		/// 接收应用 Authcode, 用于注册接口	
+		/// </summary>	
+		/// <param name="authCode">酷Q应用验证码</param>	
+		/// <returns>返回注册结果给酷Q</returns>	
+		[DllExport (ExportName = "Initialize", CallingConvention = CallingConvention.StdCall)]	
+		private static int Initialize (int authCode)	
+		{	
+			// 反射获取 AppData 实例	
+			Type appDataType = typeof (AppData);	
+			// 注册一个 CQApi 实例	
+			AppInfo appInfo = new AppInfo ("cn.zxlee.manageforfafu", 1, 9, "复读机应用 for C#", "1.0.0", 1, "Jie2GG", "WIKI所使用的 Demo", authCode);	
+			appDataType.GetRuntimeProperty ("CQApi").GetSetMethod (true).Invoke (null, new object[] { new CQApi (appInfo) });	
+			AppData.UnityContainer.RegisterInstance<CQApi> ("cn.zxlee.manageforfafu", AppData.CQApi);	
+			// 向容器注册一个 CQLog 实例	
+			appDataType.GetRuntimeProperty ("CQLog").GetSetMethod (true).Invoke (null, new object[] { new CQLog (authCode) });	
+			AppData.UnityContainer.RegisterInstance<CQLog> ("cn.zxlee.manageforfafu", AppData.CQLog);	
+			// 注册插件全局异常捕获回调, 用于捕获未处理的异常, 回弹给 酷Q 做处理	
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;	
+			// 本函数【禁止】处理其他任何代码，以免发生异常情况。如需执行初始化代码请在Startup事件中执行（Type=1001）。	
+			return 0;	
+		}	
+		#endregion	
+		
+		#region --私有方法--	
+		/// <summary>	
+		/// 全局异常捕获, 用于捕获开发者未处理的异常, 此异常将回弹至酷Q进行处理	
+		/// </summary>	
+		/// <param name="sender">事件来源对象</param>	
+		/// <param name="e">附加的事件参数</param>	
+		private static void CurrentDomain_UnhandledException (object sender, UnhandledExceptionEventArgs e)	
+		{	
+			Exception ex = e.ExceptionObject as Exception;	
+			if (ex != null)	
+			{	
+				StringBuilder innerLog = new StringBuilder ();	
+				innerLog.AppendLine ("发现未处理的异常!");	
+				innerLog.AppendLine (ex.ToString ());	
+				AppData.CQLog.SetFatalMessage (innerLog.ToString ());	
+			}	
+		}	
+		
+		/// <summary>	
+		/// 读取容器中的注册项, 进行事件分发	
+		/// </summary>	
+		private static void ResolveBackcall ()	
+		{	
+			/*	
+			 * Id: 1	
+			 * Type: 2	
+			 * Name: 群消息处理	
+			 * Function: eventGroupMsg	
+			 * Priority: 30000	
+			 */	
+			if (AppData.UnityContainer.IsRegistered<IGroupMessage> ("群消息处理"))	
+			{	
+				EventeventGroupMsgHandler += AppData.UnityContainer.Resolve<IGroupMessage> ("群消息处理").GroupMessage;	
+			}	
+			
+		}	
+		#endregion	
+		
+		#region --导出方法--	
+		/// <summary>	
+		/// 事件回调, 以下是对应 Json 文件的信息	
+		/// <para>Id: 1</para>	
+		/// <para>Type: 2</para>	
+		/// <para>Name: 群消息处理</para>	
+		/// <para>Function: eventGroupMsg</para>	
+		/// <para>Priority: 30000</para>	
+		/// <para>IsRegex: False</para>	
+		/// </summary>	
+		public static event EventHandler<CQGroupMessageEventArgs> EventeventGroupMsgHandler;	
+		[DllExport (ExportName = "eventGroupMsg", CallingConvention = CallingConvention.StdCall)]	
+		public static int EventeventGroupMsg (int subType, int msgId, long fromGroup, long fromQQ, string fromAnonymous, IntPtr msg, int font)	
+		{	
+			if (EventeventGroupMsgHandler != null)	
+			{	
+				CQGroupMessageEventArgs args = new CQGroupMessageEventArgs (AppData.CQApi, AppData.CQLog, 1, 2, "群消息处理", "eventGroupMsg", 30000, subType, msgId, fromGroup, fromQQ, fromAnonymous, msg.ToString(CQApi.DefaultEncoding), false);	
+				EventeventGroupMsgHandler (typeof (CQEventExport), args);	
+				return (int)(args.Handler ? CQMessageHandler.Intercept : CQMessageHandler.Ignore);	
+			}	
+			return 0;	
+		}	
+		
+		#endregion	
+	}	
+}
